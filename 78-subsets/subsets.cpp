7d5983b58a45155bf1832c5ec7c9d9ed29248f38
mdosama8435 <mdosama7055@gmail.com>
2026-01-15 10:35:17 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (95.18%) - LeetSync

--- 78-subsets/subsets.cpp
+++ 78-subsets/subsets.cpp
@@ -1,31 +1,44 @@
 class Solution {
 public:
 
-vector<vector<int>> result;
-vector<int> current;
+//vector<vector<int>> result;
+//vector<int> current;
 
-void backtrack(vector<int>& nums,int index){
+//void backtrack(vector<int>& nums,int index){
 
     //ase condition
-    if(index==nums.size()){
-        result.push_back(current);
-        return;
-    }
+    //if(index==nums.size()){
+        //result.push_back(current);
+        //return;
+    //}
 
     //choices-Take it
-    current.push_back(nums[index]);
-    backtrack(nums,index+1);
+    //current.push_back(nums[index]);
+    //backtrack(nums,index+1);
          
-        current.pop_back();//backtracking
+       // current.pop_back();//backtracking
 
     //Leave it
-    backtrack(nums,index+1);
+    //backtrack(nums,index+1);
 
-}
+//}
 
-    vector<vector<int>> subsets(vector<int>& nums) {
-        backtrack(nums,0);
-        return result;
+    //vector<vector<int>> subsets(vector<int>& nums) {
+        //backtrack(nums,0);
+        //return result;
         
+    //}
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> dp = {{}};
+
+        for (int num : nums) {
+            int size = dp.size();
+            for (int i = 0; i < size; i++) {
+                dp.push_back(dp[i]);
+                dp.back().push_back(num);
+            }
+        }
+        return dp;
     }
+   
 };
