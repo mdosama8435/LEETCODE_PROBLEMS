21f73ba938213885e965596bfac0dc07cc0f9418
mdosama8435 <mdosama7055@gmail.com>
2026-02-21 07:02:59 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (27.64%) - LeetSync

--- 133-clone-graph/clone-graph.cpp
+++ 133-clone-graph/clone-graph.cpp
@@ -1,51 +1,27 @@
-/*
-// Definition for a Node.
-class Node {
-public:
-    int val;
-    vector<Node*> neighbors;
-    Node() {
-        val = 0;
-        neighbors = vector<Node*>();
-    }
-    Node(int _val) {
-        val = _val;
-        neighbors = vector<Node*>();
-    }
-    Node(int _val, vector<Node*> _neighbors) {
-        val = _val;
-        neighbors = _neighbors;
-    }
-};
-*/
-
 class Solution {
 public:
+    
+    unordered_map<Node*, Node*> mp;
+    
     Node* cloneGraph(Node* node) {
-        if(node == NULL) return NULL;
-
-        unordered_map<Node*, Node*> mp;
-        queue<Node*> q;
-
-        //clone First node
+        
+        if (node == NULL) return NULL;
+        
+        // Agar node already clone ho chuka hai
+        if (mp.find(node) != mp.end())
+            return mp[node];
+        
+        // Naya clone node banao
         Node* clone = new Node(node->val);
-        mp[node]=clone;
-        q.push(node);
-
-        while(!q.empty()){
-            Node*curr = q.front();
-            q.pop();
-
-            for(Node* neighbor : curr->neighbors){
-
-                if(mp.find(neighbor) == mp.end()){
-                    mp[neighbor] = new Node(neighbor->val);
-                    q.push(neighbor);
-                }
-
-                mp[curr]->neighbors.push_back(mp[neighbor]);
-            }
+        
+        // Map me store karo
+        mp[node] = clone;
+        
+        // Neighbors clone karo recursively
+        for (Node* neighbor : node->neighbors) {
+            clone->neighbors.push_back(cloneGraph(neighbor));
         }
+        
         return clone;
     }
 };
